52454a5d860e4bf0024f293f8ffdcdd556289e80
xKakshil <154379575+xKakshil@users.noreply.github.com>
2026-04-05 20:01:40 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,17 @@ const extractRoutes = require('./routes/extractRoutes');
 dotenv.config();
 
 const app = express();
+const cors = require('cors');
+
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://benefit-analyzer.vercel.app/'
+    ],
+    credentials: true
+  })
+);
 
 app.use(cors());
 app.use(express.json());
