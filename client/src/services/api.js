7a2c37bf39897bcadd512a45a4f669bb14fc8c2b
xKakshil <154379575+xKakshil@users.noreply.github.com>
2026-04-05 07:07:27 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api'
+  baseURL: 'https://benefit-analyzer.onrender.com'
 });
 
 export const uploadClosingDisclosure = async (file) => {
